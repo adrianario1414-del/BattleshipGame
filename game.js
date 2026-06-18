741e2a04742f2d674b677c741dbbb2eac0e9e820
adrianario1414-del <adrianario1414@gmail.com>
2026-06-18 17:51:30 +0700

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ const ships = [4,3,3,2,2,2,1,1,1,1];
 
 let board = [];
 
-function createEmptyBoard(){
+function createBoard(){
 
     board = [];
 
@@ -26,38 +26,35 @@ function createEmptyBoard(){
 
 }
 
-function canPlaceShip(x, y, length, horizontal){
+function canPlace(x, y, length, horizontal){
 
-    for(let i = -1; i <= 1; i++){
+    for(let i = 0; i < length; i++){
 
-        for(let j = -1; j <= length; j++){
+        let cx = horizontal ? x + i : x;
+        let cy = horizontal ? y : y + i;
 
-            let nx;
-            let ny;
+        if(cx >= SIZE || cy >= SIZE){
 
-            if(horizontal){
+            return false;
 
-                nx = x + j;
-                ny = y + i;
+        }
 
-            }
-            else{
+        for(let yy = cy - 1; yy <= cy + 1; yy++){
 
-                nx = x + i;
-                ny = y + j;
+            for(let xx = cx - 1; xx <= cx + 1; xx++){
 
-            }
+                if(
+                    xx >= 0 &&
+                    xx < SIZE &&
+                    yy >= 0 &&
+                    yy < SIZE
+                ){
 
-            if(
-                nx >= 0 &&
-                nx < SIZE &&
-                ny >= 0 &&
-                ny < SIZE
-            ){
+                    if(board[yy][xx] === 1){
 
-                if(board[ny][nx] === 1){
+                        return false;
 
-                    return false;
+                    }
 
                 }
 
@@ -73,26 +70,16 @@ function canPlaceShip(x, y, length, horizontal){
 
 function placeShip(length){
 
-    while(true){
+    let placed = false;
+
+    while(!placed){
 
         let horizontal = Math.random() < 0.5;
 
         let x = Math.floor(Math.random() * SIZE);
         let y = Math.floor(Math.random() * SIZE);
 
-        if(horizontal && x + length > SIZE){
-
-            continue;
-
-        }
-
-        if(!horizontal && y + length > SIZE){
-
-            continue;
-
-        }
-
-        if(!canPlaceShip(x, y, length, horizontal)){
+        if(!canPlace(x, y, length, horizontal)){
 
             continue;
 
@@ -113,7 +100,7 @@ function placeShip(length){
 
         }
 
-        break;
+        placed = true;
 
     }
 
@@ -121,7 +108,7 @@ function placeShip(length){
 
 function generateShips(){
 
-    createEmptyBoard();
+    createBoard();
 
     ships.forEach(ship => {
 
@@ -160,3 +147,13 @@ function drawBoard(){
 generateShips();
 
 drawBoard();
+
+document
+.getElementById("restartBtn")
+.addEventListener("click", () => {
+
+    generateShips();
+
+    drawBoard();
+
+});
