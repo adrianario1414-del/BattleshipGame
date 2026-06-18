593991b2e5511ac09b56f0d2b190a3d95d703fcb
adrianario1414-del <adrianario1414@gmail.com>
2026-06-18 17:06:46 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,44 @@
+const playerBoard = document.getElementById("playerBoard");
+const enemyBoard = document.getElementById("enemyBoard");
+const statusText = document.getElementById("status");
 
+// создаем поля
+for(let i = 0; i < 100; i++){
+
+    // поле игрока
+    const playerCell = document.createElement("div");
+    playerCell.classList.add("cell");
+
+    // случайные корабли игрока
+    if(Math.random() < 0.15){
+        playerCell.classList.add("ship");
+    }
+
+    playerBoard.appendChild(playerCell);
+
+    // поле противника
+    const enemyCell = document.createElement("div");
+    enemyCell.classList.add("cell");
+
+    enemyCell.addEventListener("click", ()=>{
+
+        // если уже стреляли - ничего не делать
+        if(enemyCell.classList.contains("hit") ||
+           enemyCell.classList.contains("miss")){
+            return;
+        }
+
+        // случайно определяем попадание
+        if(Math.random() < 0.15){
+            enemyCell.classList.add("hit");
+            statusText.textContent = "💥 Попадание!";
+        }
+        else{
+            enemyCell.classList.add("miss");
+            statusText.textContent = "🌊 Мимо!";
+        }
+
+    });
+
+    enemyBoard.appendChild(enemyCell);
+}
